f3ddcd8bf05ad92840d77b82e4538a2f8c3f6769
carepez <mauroalzate19@gmail.com>
2014-09-15 23:24:42 +0200

    Modificato wp-config.php per il debug

--- wp-config.php
+++ wp-config.php
@@ -87,7 +87,18 @@ define('WPLANG', 'it_IT');
  * E' fortemente raccomandato agli svilupaptori di temi e plugin di utilizare
  * WP_DEBUG all'interno dei loro ambienti di sviluppo.
  */
-define('WP_DEBUG', false);
+/*define('WP_DEBUG', false);*/
+
+define('WP_DEBUG', true);
+ 
+// Tells WordPress to log everything to the /wp-content/debug.log file
+define('WP_DEBUG_LOG', true);
+ 
+// Doesn't force the PHP 'display_errors' variable to be on
+define('WP_DEBUG_DISPLAY', false);
+ 
+// Hides errors from being displayed on-screen
+@ini_set('display_errors', 0);
 
 /* Finito, interrompere le modifiche! Buon blogging. */
 
